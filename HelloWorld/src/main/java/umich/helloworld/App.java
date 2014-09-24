48bbb3cc21add53c7e9f9c5e4ac0db04423609af
Yifeng Yang <yang-yifeng@hotmail.com>
2014-09-23 22:45:21 -0400

    Maven Test HelloWorld junit test

---
+++ HelloWorld/src/main/java/umich/helloworld/App.java
@@ -0,0 +1,18 @@
+package umich.helloworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    public String talks(String name){
+    
+        String message="Hello ! "+name; 
+        return message; 
+    }
+}
